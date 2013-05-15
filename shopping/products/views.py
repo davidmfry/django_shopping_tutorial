7eff0434b17c33b5b28ac825c42f67ffdbfe59bc
David Fry <david.fry.tv@gmail.com>
2013-05-14 22:06:15 -0700

    Getting ready too add unique slugs

---
+++ shopping/products/views.py
@@ -0,0 +1,15 @@
+from datetime import date
+from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+from .models import Products
+
+def products(request):
+    queryset = Products.objects.all()
+    
+    return render_to_response('results.html', locals(), context_instance=RequestContext(request))
+
+def product_single(request,slug):
+    product = Products.objects.get(slug=slug)
+    
+    return render_to_response('product.html', locals(), context_instance=RequestContext(request))
