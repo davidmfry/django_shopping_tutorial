c2943a3103ef452f412861acb2b6a5299e00bedd
David Fry <david.fry.tv@gmail.com>
2013-05-17 08:47:22 -0700

    Forms Working

--- shopping/contact/views.py
+++ shopping/contact/views.py
@@ -1,5 +1,5 @@
 from datetime import date
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from .form import ContactUsForm
@@ -7,7 +7,8 @@ from .form import ContactUsForm
 def contact(request):
     form = ContactUsForm(request.POST or None)
     if form.is_valid():
-        this_form.save(commit = False)
+        this_form = form.save(commit = False)
         this_form.save()
+        return HttpResponseRedirect('/')
     
     return render_to_response('contact/form.html', locals(), context_instance=RequestContext(request))
